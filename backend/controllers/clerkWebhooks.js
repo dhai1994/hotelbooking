5779afa96216d54d0e347d3a7aed6a4ba5e490bd
Dhairya <rathoredhairya2004@gmail.com>
2025-08-18 02:56:15 +0530

    Update project files

--- backend/controllers/clerkWebhooks.js
+++ backend/controllers/clerkWebhooks.js
@@ -1,57 +1,52 @@
-import user from "../models/User"
-import { Webhook } from "svix"
+import User
+ from "../models/User.js";
 
-const clerkWebhooks =async(requestAnimationFrame,res)=>{
-  try {
-    //sevix instance with cler webhook secret
-    const whook =new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+ import { Webhook } from "svix";
 
-    //getting headers
-    const headers ={
-"svix-id":req.headers["svix-id"],
-"svix-timestamp":req.headers["svix-timestamp"],
-"svix-signature":req.headers["svix-signature"],
+ const clerkWebhooks = async (req , res)=>{
+    try{
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+        const headers = {
+            "svix-id":req.headers["svix-id"],
+            "svix-timestamp":req.headers["svix-timestamp"],
+            "svix-signature":req.headers["svix-signature"]
+        };
+
+        await whook.verify(JSON.stringify(req.body),headers)
+
+        const {data , type} = req.body
+
+        const userData = {
+            _id : data.id,
+            email:data.email_addresses[0].email_address,
+            username : data.first_name + " " + data.last_name,
+            image:data.image_url,
+        }
+
+        switch(type) {
+            case "user.created":{
+                await User.create(userData)
+                break;
+            }
+
+            case "user.updated":{
+                await User.findByIdAndUpdate(data.id , userData)
+                break;
+            }
+
+            case "user.deleted":{
+                await User.findByIdAndDelete(data.id);
+                break;
+            }
+
+            default:
+                break;
+        }
+        res.json({success : true , message : "Webhook Recieved"})
+    } catch(error){
+        console.log(error.message);
+        res.json({success:false , message:error.message});
+    }
 }
-
-//verifying headers
-await whook,verify(JSON.stringify(req.body),headers)
-
-//getting data from request body
-const {data,type} =req.body
-
-const userData ={
-  _id:data.id,
-  emial:data.email_addresses[0].email_address,
-  username:data.first_name+""+data.last_name,
-  image:data.image.url,
-
-}
-
-switch(type) {
-case "user.created":{
-  await User.create(userData)
-break;}
-case "user.updated":{
-    await User.findByIdAndUpdate(data.id , userData)
-        break;
-}
-
-case "user.deleted":{
-    await User.findByIdAndDelete(data.id);
-    break;
-}
-
-default:
-break;
-}
-
-res.json({success : true , message : "Webhook Recieved"})
-
-  } catch (error){
-console.log(error.message);
-res.json({success:false,message:error.message})
-  }
-}
-
-
-export default clerkWebhooks
+export default clerkWebhooks;
